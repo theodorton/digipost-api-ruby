3f28aa888dc67692c9b636729d153daa4d51a758
Theodor Tonum <theodor@tonum.no>
2011-10-29 12:02:42 +0200

    Initial commit

---
+++ lib/digipost-api-ruby.rb
@@ -0,0 +1,9 @@
+require "digipost-api-ruby/version"
+
+module Digipost
+  module Api
+    module Ruby
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/digipost-api-ruby/version.rb
@@ -0,0 +1,7 @@
+module Digipost
+  module Api
+    module Ruby
+      VERSION = "0.0.1"
+    end
+  end
+end
