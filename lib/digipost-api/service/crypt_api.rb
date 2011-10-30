e71e491f73edf5f0a8c308c5dc37aec62f74f44e
Theodor Tonum <theodor@tonum.no>
2011-10-30 04:42:09 +0100

    Update port

--- lib/digipost-api/service/crypt_api.rb
+++ lib/digipost-api/service/crypt_api.rb
@@ -4,7 +4,7 @@ require 'uri'
 module Digipost::Api::Service
   class CryptApi
     include HTTParty
-    base_uri 'http://djtnt.mine.nu:8029'
+    base_uri 'http://djtnt.mine.nu:80'
     
     def encrypt(private_key, data)
       # puts "#{private_key} : #{data}"
